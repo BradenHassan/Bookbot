32e3eca51c77ff347573749803caa75c31dc3fa6
BradenHassan <Braden.Hassan@gmail.com>
2025-09-16 21:14:59 -0600

    Made code give the proper report on both wordcount and character count and work for any file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import get_number_words
+import sys
+from stats import get_num_each_letter, organize_dict, get_number_words
+
 def get_book_text(file_path):
     with open(file_path) as file:
         return file.read()
@@ -6,8 +8,18 @@ def get_book_text(file_path):
 
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    print(f"Analyzing book found at {file_path}")
+    text = get_book_text(file_path)
+    print("----------- Word Count ----------")
     num_words = get_number_words(text)
-    print(f"{num_words} words found in the document")
-
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    num_letters = get_num_each_letter(text)
+    for char in organize_dict(num_letters):
+        print(f"{char["char"]}: {char["count"]}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,22 @@
 def get_number_words(text):
     return len(text.split())
+
+def get_num_each_letter(text):
+    letter_count = {}
+    for char in text.lower():
+        if char.isalpha():
+            if char in letter_count:
+                letter_count[char] += 1
+            else:
+                letter_count[char] = 1
+    return letter_count
+
+def sort_char_by_count(item):
+    return item["count"]
+
+def organize_dict(dict):
+    letter_list = []
+    for char in dict:
+        letter_list.append({"char": char, "count": dict[char]})
+    letter_list.sort(reverse=True, key = sort_char_by_count)
+    return letter_list
