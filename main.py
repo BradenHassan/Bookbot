66bfab0887557596cccaba82987bd4b810d296c0
BradenHassan <Braden.Hassan@gmail.com>
2025-09-16 20:33:11 -0600

    moved word count function to different file ignore pycache

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-
+from stats import get_number_words
 def get_book_text(file_path):
     with open(file_path) as file:
         return file.read()
 
-def get_number_words(text):
-    return len(text.split())
+
 
 def main():
     text = get_book_text("books/frankenstein.txt")
