ed49d144eb3bf6728648858ec8cc5765c282a4a6
BradenHassan <Braden.Hassan@gmail.com>
2025-09-16 20:27:31 -0600

    counted the number of words in frankenstein.txt in books.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+
+def get_book_text(file_path):
+    with open(file_path) as file:
+        return file.read()
+
+def get_number_words(text):
+    return len(text.split())
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    num_words = get_number_words(text)
+    print(f"{num_words} words found in the document")
+
+main()
